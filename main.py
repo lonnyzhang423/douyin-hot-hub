5e57816ddfaea914c28074175302f4cd22d7a0bc
lonny <lonnyzhang423@gmail.com>
2021-01-09 17:48:58 +0800

    update

--- main.py
+++ main.py
@@ -1,56 +1,9 @@
 import json
-import logging
 import os
 import traceback
-import urllib.parse
-
-import requests
-from requests.adapters import HTTPAdapter
-from urllib3.util.retry import Retry
 
 import util
-
-logging.basicConfig()
-log = logging.getLogger(__name__)
-log.setLevel(level=logging.INFO)
-
-# HOT_UR='https://is-lq.snssdk.com/api/suggest_words/?business_id=10016'
-HOT_URL = "https://i-lq.snssdk.com/api/feed/hotboard_online/v1/?category=hotboard_online&count=50"
-
-retries = Retry(total=2,
-                backoff_factor=0.1,
-                status_forcelist=[k for k in range(400, 600)])
-
-
-def getContent(url: str) -> str:
-    try:
-        with requests.session() as s:
-            s.mount("http://", HTTPAdapter(max_retries=retries))
-            s.mount("https://", HTTPAdapter(max_retries=retries))
-            return s.get(url).text
-    except:
-        log.error(traceback.format_exc())
-
-
-def parseSearchList(content):
-    """解析话题
-    """
-    def search(item):
-        content = json.loads(item['content'])
-        title = content['raw_data']['title']
-        url = 'https://so.toutiao.com/search?keyword={}'.format(
-            urllib.parse.quote(title))
-        info = {'title': title, 'url': url}
-        return info
-
-    result = []
-    try:
-        data = json.loads(content)['data']
-        result = [search(item) for item in data]
-    except:
-        log.error(traceback.format_exc())
-
-    return result
+from util import logger
 
 
 def generateArchiveMd(items):
@@ -64,12 +17,12 @@ def generateArchiveMd(items):
         searchs = '\n'.join([search(item) for item in items])
 
     readme = ''
-    file = os.path.join('template','archive.md')
+    file = os.path.join('template', 'archive.md')
     with open(file) as f:
         readme = f.read()
 
-    date = util.currentDateStr()
-    now = util.currentTimeStr()
+    date = util.current_date()
+    now = util.current_time()
     readme = readme.replace("{date}", date)
     readme = readme.replace("{updateTime}", now)
     readme = readme.replace("{searches}", searchs)
@@ -88,11 +41,11 @@ def generateReadme(items):
         searchs = '\n'.join([search(item) for item in items])
 
     readme = ''
-    file = os.path.join('template','README.md')
+    file = os.path.join('template', 'README.md')
     with open(file) as f:
         readme = f.read()
 
-    now = util.currentTimeStr()
+    now = util.current_time()
     readme = readme.replace("{updateTime}", now)
     readme = readme.replace("{searches}", searchs)
 
@@ -100,22 +53,22 @@ def generateReadme(items):
 
 
 def handleTodayMd(md):
-    log.debug('today md:%s', md)
-    util.writeText('README.md', md)
+    logger.debug('today md:%s', md)
+    util.write_text('README.md', md)
 
 
 def handleArchiveMd(md):
-    log.debug('archive md:%s', md)
-    name = util.currentDateStr()+'.md'
+    logger.debug('archive md:%s', md)
+    name = util.current_date()+'.md'
     file = os.path.join('archives', name)
-    util.writeText(file, md)
+    util.write_text(file, md)
 
 
 def handleRawContent(content: str):
-    log.debug('raw content:%s', content)
-    name = util.currentDateStr()+'.json'
+    logger.debug('raw content:%s', content)
+    name = util.current_date()+'.json'
     file = os.path.join('raw', name)
-    util.writeText(file, content)
+    util.write_text(file, content)
 
 
 def run():
